7e8816d1c7666ad98723ec7a5e0f301e38d6d4a9
Frank Su <candyline25@gmail.com>
2020-01-23 23:53:01 -0800

    Initial Commit

---
+++ SwiftyStarwars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyStarwars
+//
+//  Created by Frank Su on 2020-01-23.
+//  Copyright © 2020 frankusu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
