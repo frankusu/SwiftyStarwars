69879e566bdcf60727836f63a03040101d7622fc
Frank Su <candyline25@gmail.com>
2020-01-26 10:33:08 -0800

    Add sections to Film Details

--- SwiftyStarwars/Controller/FilmDetailController.swift
+++ SwiftyStarwars/Controller/FilmDetailController.swift
@@ -12,20 +12,38 @@ class FilmDetailController: UITableViewController {
 
     fileprivate let cellId = "category"
     
-    let categories = ["LukeSkywalker","Yoda Boi"," Obi wan my true one","Chubaka","Piew piew",]
+    let characters = ["LukeSkywalker","Yoda Boi"," Obi wan my true one","Chubaka","Piew piew",]
+    let vehicles = ["deathStar?","x-wing", "bmw x3", "GTR"]
+    let planets = ["Arrakis","Death Star","Pluto"]
+    lazy var twoDimensionArray = [
+        characters,
+        vehicles,
+        planets
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Film Detail"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        label.text = "HEADER"
+        return label
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return twoDimensionArray.count
+    }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        cell.textLabel?.text = categories[indexPath.row]
+        cell.textLabel?.text = twoDimensionArray[indexPath.section][indexPath.row]
         return cell
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        return twoDimensionArray[section].count
     }
 
     
